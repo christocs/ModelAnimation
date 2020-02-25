e3ea536fd8614fe752f4ad87642815f9c791908c
Sandro Stikić <sandro@stikic.me>
2020-02-25 17:10:50 +0800

    Add SFML

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
-auto main(int argc, char **argv) -> int {
-    std::cout << "Hello world!\n";
+#include <SFML/Window.hpp>
 
-    return 0;
+using sf::Window;
+
+auto main() -> int {
+    auto window = sf::Window{sf::VideoMode(800, 600), "ICT397"};
+
+    while (window.isOpen()) {
+        auto event = sf::Event{};
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
 }
