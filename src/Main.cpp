145949dbee7d21df5c5f18228c4c478379ec1aa6
Sandro Stikić <sandro@stikic.me>
2020-03-02 14:40:12 +0800

    Add assimp to cmake

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 #include <SFML/Window.hpp>
+#include <assimp/Importer.hpp>
+#include <entt/entt.hpp>
 
 using sf::Window;
 
 auto main() -> int {
-    auto window = sf::Window{sf::VideoMode(800, 600), "ICT397"};
+    auto window   = sf::Window{sf::VideoMode(800, 600), "ICT397"};
+    auto importer = Assimp::Importer{};
 
     while (window.isOpen()) {
         auto event = sf::Event{};
