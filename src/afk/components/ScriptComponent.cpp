3a71179b6f5e17ac599826404453a12772faf38b
jammy-dodgers <emily@jambox.cc>
2020-03-18 11:38:15 +0800

    Add constructor

---
+++ src/afk/components/ScriptComponent.cpp
@@ -0,0 +1,20 @@
+#include "ScriptComponent.hpp"
+
+#include <stdexcept>
+
+ScriptComponent::ScriptComponent(lua_State *lua, std::string filename)
+    : scriptFilename(filename) {
+    if (luaL_dofile(lua, scriptFilename.c_str()) != 0) {
+        throw std::runtime_error{"Error loading " + filename + ": " +
+                                 lua_tostring(lua, -1)};
+    }
+    this->onUpdate       = luabridge::getGlobal(lua, "update");
+    this->onDraw         = luabridge::getGlobal(lua, "draw");
+    this->onKeyPress     = luabridge::getGlobal(lua, "keyDown");
+    this->onKeyRelease   = luabridge::getGlobal(lua, "keyUp");
+    this->onMouseMove    = luabridge::getGlobal(lua, "mouseMove");
+    this->onMousePress   = luabridge::getGlobal(lua, "mouseDown");
+    this->onMouseRelease = luabridge::getGlobal(lua, "mouseUp");
+    this->onMouseScroll  = luabridge::getGlobal(lua, "mouseScroll");
+    this->onTextEnter    = luabridge::getGlobal(lua, "textEnter");
+}
