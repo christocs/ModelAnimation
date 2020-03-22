2681e770323b4dc3de7570ac6925ac94d7cce34a
jammy-dodgers <emily@jambox.cc>
2020-03-22 21:15:56 +0800

    Add comments to ScriptKeyboard and ScriptMouse

--- src/afk/components/ScriptKeyboard.hpp
+++ src/afk/components/ScriptKeyboard.hpp
@@ -3,10 +3,18 @@
 #include <vector>
 
 #include <SFML/Window/Keyboard.hpp>
+/**
+ * Basically acts as a façade between sf::Keyboard and Lua
+ */
 namespace LuaKeyboard {
   struct Key {
     std::string name;
     int code;
   };
+  /**
+   * Return not const as references to these values are used by lua.
+   * Despite being "readonly" from lua's side, they can't be const.
+   * (Even readonly variables can be changed in lua, with things like rawset.)
+   */
   auto getKeys() -> std::vector<Key> &;
 }
