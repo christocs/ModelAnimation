fac7be4f62bcf8c92f3a60c4380b8bb0fc0d1e6f
Sandro Stikić <sandro@stikic.me>
2020-04-06 16:32:43 +0800

    Tidy main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,6 @@
 #include <cstdlib>
-#include <iostream>
 
 #include "afk/Afk.hpp"
-#include "afk/renderer/ShaderProgram.hpp"
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) -> int {
   auto &afk = Afk::Engine::get();
