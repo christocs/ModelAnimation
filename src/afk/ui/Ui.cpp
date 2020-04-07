212fcee8cdd5dc40d8723d53840eb98a648bfc0a
Sandro Stikić <sandro@stikic.me>
2020-04-08 00:33:55 +0800

    Add UI DPI scaling

--- src/afk/ui/Ui.cpp
+++ src/afk/ui/Ui.cpp
@@ -5,25 +5,36 @@
 #include <imgui/imgui.h>
 
 #include "afk/Afk.hpp"
+#include "afk/io/Log.hpp"
+#include "afk/io/Path.hpp"
 #include "afk/renderer/Renderer.hpp"
 #include "afk/utility/Assert.hpp"
 
 using Afk::Engine;
 using Afk::Ui;
 
-Ui::Ui(Renderer::Window _window) : window(_window) {
+Ui::Ui(Renderer::Window _window)
+  : ini_path(Afk::get_absolute_path("cfg/imgui.ini").string()), window(_window) {
   afk_assert(_window != nullptr, "Window is uninitialized");
 
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   auto &io = ImGui::GetIO();
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  Afk::status << this->ini_path << '\n';
+  io.IniFilename = this->ini_path.c_str();
+  // ImGui::LoadIniSettingsFromDisk(this->ini_path.c_str());
   ImGui::StyleColorsDark();
   ImGui_ImplGlfw_InitForOpenGL(this->window, true);
   ImGui_ImplOpenGL3_Init("#version 410");
+  io.Fonts->AddFontFromFileTTF(
+      Afk::get_absolute_path("res/font/NotoSans-Regular.ttf").string().c_str(), 19.0f);
+  auto &style = ImGui::GetStyle();
+  style.ScaleAllSizes(1.5f);
 }
 
 Ui::~Ui() {
+  // ImGui::SaveIniSettingsToDisk(this->ini_path.c_str());
   ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
--- src/afk/ui/Ui.hpp
+++ src/afk/ui/Ui.hpp
@@ -14,6 +14,7 @@ namespace Afk {
 
     auto draw() -> void;
 
+    std::string ini_path    = {};
     bool is_visible         = false;
     Renderer::Window window = {};
   };
