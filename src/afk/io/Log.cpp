47dbfd6dcc640fb507b32ddf30e106de1194b936
Sandro Stikić <sandro@stikic.me>
2020-04-12 01:23:42 +0800

    more pain

--- src/afk/io/Log.cpp
+++ src/afk/io/Log.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <string>
 
 #include "afk/debug/Assert.hpp"
 
@@ -11,13 +13,16 @@ using Afk::Io::Log;
 using std::ofstream;
 using std::ostringstream;
 using std::filesystem::path;
+using namespace std::string_literals;
 
 Log::Log() {
+  // FIXME: Make paths native per OS.
   auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
   auto ss = ostringstream{};
-  ss << std::put_time(std::localtime(&t), "%T-%F") << ".log";
+  ss << ".log/" << std::put_time(std::localtime(&t), "%T-%F") << ".log";
 
-  this->log_path = Afk::get_absolute_path(".log") / ss.str();
+  this->log_path = Afk::get_absolute_path(ss.str());
+  std::cout << this->log_path << '\n';
   std::filesystem::create_directory(this->log_path.parent_path());
   this->log_file = ofstream{this->log_path};
   afk_assert(this->log_file.is_open(), "Failed to open log file");
