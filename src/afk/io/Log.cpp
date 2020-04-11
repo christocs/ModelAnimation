aa43e7a8175cc3716ba80390bbcd0d5db4ea6335
Sandro Stikić <sandro@stikic.me>
2020-04-11 23:39:38 +0800

    Add log file

--- src/afk/io/Log.cpp
+++ src/afk/io/Log.cpp
@@ -1,3 +1,22 @@
 #include "afk/io/Log.hpp"
 
-Afk::Io::Log Afk::Io::log = {};
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+
+using Afk::Io::Log;
+
+using std::ofstream;
+using std::ostringstream;
+using std::filesystem::path;
+
+Log::Log() {
+  auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  auto ss = ostringstream{};
+  ss << std::put_time(std::localtime(&t), "%T-%F") << ".log";
+
+  this->log_path = Afk::get_absolute_path(".log") / ss.str();
+  std::filesystem::create_directory(this->log_path.parent_path());
+  this->log_file = ofstream{this->log_path};
+  afk_assert(this->log_file.is_open(), "Failed to open log file");
+}
--- src/afk/io/Log.hpp
+++ src/afk/io/Log.hpp
@@ -1,25 +1,37 @@
 #pragma once
 
+#include <filesystem>
+#include <fstream>
 #include <sstream>
 
 #include "afk/Afk.hpp"
+#include "afk/debug/Assert.hpp"
+#include "afk/io/Path.hpp"
 #include "afk/ui/Log.hpp"
 
 namespace Afk {
   namespace Io {
-    struct Log {};
+    struct Log {
+      std::filesystem::path log_path = {};
+      std::ofstream log_file         = {};
+
+      Log();
+    };
 
     template<typename T>
     auto operator<<(Log &log, T const &value) -> Log & {
-      auto &afk = Engine::get();
+      using std::ostringstream;
 
-      auto ss = std::ostringstream{};
+      auto &afk = Engine::get();
+      auto ss   = ostringstream{};
+
       ss << value;
       afk.ui.log.append("%s", ss.str().c_str());
+      log.log_file << value;
 
       return log;
     }
 
-    extern Log log;
+    inline auto log = Log{};
   }
 }
