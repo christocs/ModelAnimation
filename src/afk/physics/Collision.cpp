ab0a504927fbe8857f656e01fe292d158e3417fe
Christo Stephenson <christo@blueskylabs.com.au>
2020-04-12 13:20:58 +0800

    Code style update

--- src/afk/physics/Collision.cpp
+++ src/afk/physics/Collision.cpp
@@ -27,12 +27,10 @@ Collision::Collision(rp3d::DynamicsWorld* world, Afk::Transform transform, float
   const auto scaleFactor = (transform.scale.x + transform.scale.y + transform.scale.z) / 3.0f;
   this->collisionShape = std::make_unique<rp3d::SphereShape>(boundingSphere * scaleFactor);
 
-  auto transform_ = rp3d::Transform(
+  this->body = world->createRigidBody(rp3d::Transform(
     rp3d::Vector3(transform.translation[0], transform.translation[1], transform.translation[2]),
     rp3d::Quaternion(transform.rotation[0], transform.rotation[1], transform.rotation[2], transform.rotation[3])
-  );
-
-  this->body = world->createRigidBody(transform_);
+  ));
 
   this->body->enableGravity(gravity);
   this->body->setType(bodyType);
