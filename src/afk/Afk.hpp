66a1006ee04e9432b0f44396a11183d49423de72
jammy-dodgers <emily@jambox.cc>
2020-05-01 09:07:48 +0800

    Make phys body system public

--- src/afk/Afk.hpp
+++ src/afk/Afk.hpp
@@ -21,6 +21,7 @@ namespace Afk {
     Camera camera                  = {};
     TerrainManager terrain_manager = {};
     entt::registry registry;
+    Afk::PhysicsBodySystem physics_body_system;
 
     Engine()               = default;
     Engine(Engine &&)      = delete;
@@ -48,7 +49,5 @@ namespace Afk {
     bool is_running     = true;
     int frame_count     = {};
     float last_update   = {};
-
-    Afk::PhysicsBodySystem physics_body_system;
   };
 }
